01844ae64bc20f746c3d548a6c619b871bfa790e
Yugander <yugander9010@gmail.com>
2025-04-21 16:56:36 +0530

    Modified the bucket code

--- infra/__main__.py
+++ infra/__main__.py
@@ -5,7 +5,8 @@ bucket = s3.BucketV2('my-bucket')
 
 s3.BucketAclV2("my-bucket-acl",
     bucket=bucket.id,
-    acl="private" 
+    acl="private"
+    opts=pulumi.ResourceOptions(depends_on=[bucket])  # <- Explicit dependency 
 )
 
 pulumi.export('bucket_name', bucket.id)
